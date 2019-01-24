210f9366e0d8dfac50a636e57ac02ee41b422db1
ilizakoski <ilizakoski@gmail.com>
2019-01-24 08:53:52 -0200

    Create text-area

---
+++ src/text_area.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class TextArea extends React.Component {
+
+    render() {
+        return (
+            <label>{ this.props.teste }</label>
+
+        )
+    }
+}
+
+export default TextArea
